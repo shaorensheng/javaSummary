5b508db51be3a5685cd79c78576d506c30fd3ad4
shaorensheng <329917086@qq.com>
2019-03-28 10:43:36 +0800

    简单（静态）工厂模式代码

---
+++ code/staticFactoryMethod/Customer.java
@@ -0,0 +1,31 @@
+package staticFactoryMethod;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ * 顾客
+ *
+ * @author shaorensheng
+ * @date 2019/3/18 14:17
+ */
+public class Customer {
+
+    /**
+     * 买蛋糕
+     * @param cakeName
+     * @return
+     */
+    public Cake buyCake(String cakeName){
+        return CakeFactory.createCake(cakeName);
+    }
+
+    public static void main(String[] args) throws ParseException {
+        Customer customer = new Customer();
+        Cake cake = customer.buyCake("巧克力蛋糕");
+        System.out.println("顾客买到了"+cake.toString());
+    }
+
+}
